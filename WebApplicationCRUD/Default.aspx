1826b2d477d38a51052bf502f4869a438fff56e3
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-09-13 21:57:50 +0700

    Add project files.

---
+++ WebApplicationCRUD/Default.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationCRUD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="margin-bottom: 43px">
+        <div style ="font-size:x-large" align ="center">Student Info Manage Form</div>
+        <br />
+
+        <table class="nav-justified">
+            <tr>
+                <td style="width: 164px">&nbsp;</td>
+                <td style="width: 212px">Student ID</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server" Width="195px"></asp:TextBox>
+                     &nbsp
+                    <asp:Button ID="Button5" runat="server" Font-Bold="True" ForeColor="#3333FF" OnClick="Button5_Click" Text="Get" />
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 164px; height: 22px;"></td>
+                <td style="width: 212px; height: 22px;">Student Name</td>
+                <td style="height: 22px">
+                    <asp:TextBox ID="TextBox2" runat="server" Width="195px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 164px">&nbsp;</td>
+                <td style="width: 212px">Address</td>
+                <td>
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem>Batuaji</asp:ListItem>
+                        <asp:ListItem>Batam Center</asp:ListItem>
+                        <asp:ListItem>Bengkong</asp:ListItem>
+                        <asp:ListItem>Tj.Piayu</asp:ListItem>
+                        <asp:ListItem>Marina</asp:ListItem>
+                        <asp:ListItem>Nongsa</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 164px">&nbsp;</td>
+                <td style="width: 212px">Age</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server" Width="195px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 164px">&nbsp;</td>
+                <td style="width: 212px">Contact</td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server" Width="195px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="height: 62px; width: 164px"></td>
+                <td style="width: 212px; height: 62px"></td>
+                <td style="height: 62px">
+                    <asp:Button ID="Button1" runat="server" Font-Bold="True" ForeColor="#00CC00" OnClick="Button1_Click" Text="Insert" />
+                    &nbsp
+                    <asp:Button ID="Button2" runat="server" Font-Bold="True" ForeColor="#FF9900" OnClick="Button2_Click" Text="Update" />
+                     &nbsp
+                    <asp:Button ID="Button3" runat="server" Font-Bold="True" ForeColor="Red" OnClick="Button3_Click" OnClientClick="return confirm('Apa kamu yakin akan menghapus data ini??')" Text="Delete" />
+                    &nbsp
+                    <asp:Button ID="Button4" runat="server" Font-Bold="True" ForeColor="#3333FF" OnClick="Button4_Click" Text="Search" />
+                    &nbsp
+                    <asp:Button ID="Button6" runat="server" Font-Bold="True" ForeColor="#3333FF" OnClick="Button6_Click" Text="Clear" />
+                </td>
+            </tr>
+            <tr>
+                <td style="height: 62px; width: 164px">&nbsp;</td>
+                <td style="width: 212px; height: 62px">&nbsp;</td>
+                <td style="height: 62px">
+                    <asp:GridView ID="GridView1" runat="server" Width="601px">
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+</asp:Content>
---
+++ WebApplicationCRUD/Default.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationCRUD
+{
+    public partial class _Default : Page
+    {
+        //menampilkan halaman
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                LoadRecord();
+            }
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=martin-pc\martin880;Initial Catalog=StudentDB;Integrated Security=True");
+        // Cara Insert data
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=martin-pc\martin880;Initial Catalog=StudentDB;Integrated Security=True");
+            conn.Open();
+            SqlCommand comm = new SqlCommand("insert into StudentInfoTab values ('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+DropDownList1.SelectedValue+"','"+double.Parse(TextBox3.Text)+"','"+TextBox4.Text+"')", conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Berhasil memasukkan data');", true);
+            LoadRecord();
+            ClearData();
+        }
+
+        // Membuat method untuk menampilkan record di GridView
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("Select * From StudentInfoTab", conn);
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        // method untuk fungsi clear data
+        void ClearData() 
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            LoadRecord();
+        }
+
+        // fungsi update data
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand comm = new SqlCommand("update StudentInfoTab set studentName = '"+TextBox2.Text+"', address = '"+DropDownList1.SelectedValue+"', age = '"+double.Parse(TextBox3.Text)+"', contact = '"+TextBox4.Text+"' where studentId = '"+int.Parse(TextBox1.Text)+"'" , conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Berhasil mengupdate data');", true);
+            LoadRecord();
+            ClearData();
+        }
+
+        // fungsi delete data
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand comm = new SqlCommand("delete StudentInfoTab where studentId = '" + int.Parse(TextBox1.Text) + "'", conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Berhasil menghapus data');", true);
+            LoadRecord();
+        }
+
+        // fungsi Get data
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("Select * From StudentInfoTab where studentId = '" + int.Parse(TextBox1.Text) + "'", conn);
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        // fungsi Search Data
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand comm = new SqlCommand("Select * From StudentInfoTab where studentId = '" + int.Parse(TextBox1.Text) + "'", conn);
+            SqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read()) 
+            {
+                TextBox2.Text = dr.GetValue(1).ToString();
+                DropDownList1.SelectedValue = dr.GetValue(2).ToString();
+                TextBox3.Text = dr.GetValue(3).ToString();
+                TextBox4.Text = dr.GetValue(4).ToString();
+            }
+        }
+
+        // fungsi clear data
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+    }
+}
